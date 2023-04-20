18d43a6847f8f676c0b4fb64c172f9e7c4c2725c
salukasr <124944834+salukasr@users.noreply.github.com>
2023-04-20 18:27:19 +0530

    Add files via upload

---
+++ bottom-nav-bar.php
@@ -0,0 +1,22 @@
+<div class="icons">
+		<div class="icon" onclick="window.location.href = 'home.php';">
+			<i class="fa fa-home"></i>
+			<span>Home</span>
+		</div>
+		<div class="icon">
+			<i class="fa fa-history"></i>
+			<span>Reports</span>
+		</div>
+		<div class="icon">
+			<i class="fa fa-bell"></i>
+			<span>Notifications</span>
+		</div>
+		<div class="icon" onclick="window.location.href = 'profile.php';">
+			<i class="fa fa-user"></i>
+			<span>Profile</span>
+		</div>
+		<div class="icon" onclick="window.location.href = 'contactus.php';">
+			<i class="fa fa-phone"></i>
+			<span>Contact</span>
+		</div>
+</div>
