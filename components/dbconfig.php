9788a19a5a5c31c37b4fae6e247232d016be7501
salukasr <124944834+salukasr@users.noreply.github.com>
2023-04-20 18:30:31 +0530

    Update dbconfig.php

--- components/dbconfig.php
+++ components/dbconfig.php
@@ -1,1 +1,13 @@
+<?php
+    require 'C:/xampp/htdocs/SmartAid/components/vendor/autoload.php';
 
+    use Kreait\Firebase\Factory;
+    use Kreait\Firebase\ServiceAccount;
+
+    $factory = (new Factory)
+        ->withServiceAccount('C:\xampp\htdocs\SmartAid\components\smartaid-de891-firebase-adminsdk-rgc7j-6669d88888.json')
+        ->withDatabaseUri('https://smartaid-de891-default-rtdb.asia-southeast1.firebasedatabase.app');
+
+    $database = $factory->createDatabase();
+
+?>
